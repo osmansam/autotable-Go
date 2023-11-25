184bf96bdb4151ca533941f4b4869644e565f815
Osman <114867365+osmansam@users.noreply.github.com>
2023-11-24 19:26:33 -0600

    rate limit set

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"log"
 	"os"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
 	"github.com/osmansam/autotableGo/configs"
 	"github.com/osmansam/autotableGo/routes"
 )
@@ -21,6 +23,18 @@ func main() {
 		if _, err := os.Stat("./temp"); os.IsNotExist(err) {
     os.Mkdir("./temp", 0755)
 }
+	// Global rate limiting middleware
+	app.Use(limiter.New(limiter.Config{
+		Max:        100, // Set the maximum number of requests per client
+		Expiration: 1 * time.Minute, // Set the time duration for the rate limit
+		KeyGenerator: func(c *fiber.Ctx) string {
+			return c.IP() // Use client IP as the identifier
+		},
+		LimitReached: func(c *fiber.Ctx) error {
+			return c.Status(fiber.StatusTooManyRequests).SendString("Rate limit exceeded")
+		},
+	}))
+
 	//cors
 	app.Use(cors.New())
 	//run database
