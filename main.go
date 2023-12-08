c2cc73b211242e15891024efa5ee6df3c68a7385
Osman <114867365+osmansam@users.noreply.github.com>
2023-12-08 15:56:43 -0600

    execute is working

--- main.go
+++ main.go
@@ -21,46 +21,61 @@ import (
 const portNumber = ":3002"
 // executeDynamicCode executes dynamic code from a request.
 func ExecuteDynamicCode(c *fiber.Ctx) error {
-    // Structure to hold incoming request data
     type request struct {
         Code string `json:"code"`
     }
     var req request
-	 if err := c.BodyParser(&req); err != nil {
+    if err := c.BodyParser(&req); err != nil {
         return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
     }
-   functionName := c.Query("functionName")
+
+    functionName := c.Query("functionName")
     pluginFileName := "temp_" + functionName + ".so"
     fileName := "temp_" + functionName + ".go"
 
-    // Delete existing files if they exist to ensure fresh compilation
-    if _, err := os.Stat(fileName); err == nil {
-        os.Remove(fileName)
-        os.Remove(pluginFileName)
+    var p *plugin.Plugin
+    var f plugin.Symbol
+    var err error
+
+    // Check if the plugin already exists
+    p, err = plugin.Open(pluginFileName)
+    if err == nil {
+        // Plugin exists, try to lookup the function
+        f, err = p.Lookup(functionName)
+        if err == nil {
+            // Function found, execute it
+            if executeFunc, ok := f.(func(*fiber.Ctx) (interface{}, error)); ok {
+                result, err := executeFunc(c)
+                if err != nil {
+                    return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Function execution failed", "details": err.Error()})
+                }
+                return c.JSON(fiber.Map{"result": result})
+            }
+        }
     }
 
-    // Write the new code to a .go file
-    err := ioutil.WriteFile(fileName, []byte(req.Code), 0644)
+    // If the plugin doesn't exist or function not found, generate new code
+    err = ioutil.WriteFile(fileName, []byte(req.Code), 0644)
     if err != nil {
         return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Failed to write code"})
     }
 
-    // Compile the new code into a plugin
+    // Compile the code into a plugin
     out, err := exec.Command("go", "build", "-buildmode=plugin", fileName).CombinedOutput()
     if err != nil {
         return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Failed to compile code", "output": string(out)})
     }
 
     // Load the newly compiled plugin
-    p, err := plugin.Open(pluginFileName)
+    p, err = plugin.Open(pluginFileName)
     if err != nil {
         return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Failed to open plugin"})
     }
 
     // Lookup for the function in the new plugin
-    f, err := p.Lookup(functionName)
+    f, err = p.Lookup(functionName)
     if err != nil {
-        return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Failed to find function in code"})
+        return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Failed to find function in new code"})
     }
 
     // Execute the function
@@ -74,6 +89,7 @@ func ExecuteDynamicCode(c *fiber.Ctx) error {
         return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Invalid function signature"})
     }
 }
+
 func main() {
 
 	app := fiber.New()
